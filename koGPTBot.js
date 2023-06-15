efbafa41669cbf93d17044e69a771c8352d673c3
Leejinuk123 <50895677+Leejinuk123@users.noreply.github.com>
2023-06-15 14:52:02 +0900

    Update and rename kalingTest.js to koGPTBot.js
    koGPT 일단 돌아는 가는 버전

--- koGPTBot.js
+++ koGPTBot.js
@@ -1,28 +1,16 @@
 // const scriptName = "고미봇";
 // //카카오링크 관련 모듈 선언부-----------------
-//const kalingModule = require('kaling').Kakao();
-//const Kakao = new kalingModule();
 //Kakao.init('2642767733346608749b39371caaabc7');
-const REST_API_KEY = "8efcbd9e5bfe39410994c53a66fa92d7";
-//Kakao.login('leejinouk123@gmail.com', 'djswpsk1!');
-
-
-
+const REST_API_KEY = "8efcbd9e5bfe39410994c53a66fa92d7"; //REST API KEY값
 const scriptName = "kakao_link_test";
-// const kalingModule = require('kaling').Kakao();
-// const Kakao = new kalingModule();
-// importClass(org.jsoup.Jsoup);
-
-// Kakao.init(''); // 자바스크립트 키
-// Kakao.login('', '');  // 카카오 계정 아이디와 비밀번호
-
+var messageOut = "";
 function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName) {
   function getKoGPTResponse(msg) {
-    replier.reply("확인용0");
+    //replier.reply("확인용0");
     let json;
     let result;
     try {
-replier.reply("확인용4"+ msg);
+        replier.reply("확인용4"+ msg);
         let data = {
             "max_tokens": 16,
             "temperature": 0.9,
@@ -30,7 +18,6 @@ replier.reply("확인용4"+ msg);
             "n": 1,
             "prompt": msg
         };
-//{\"max_tokens\":16,\"temperature\":1,\"top_p\":1,\"n\":1,\"prompt\":\"안녕하세요\"}"
         let response = org.jsoup.Jsoup.connect("https://api.kakaobrain.com/v1/inference/kogpt/generation")
             .header("Authorization", "KakaoAK " + REST_API_KEY) // Open ai 토큰값 Authorization: KakaoAK {REST_API_KEY}
             .header("Content-Type", "application/json")
@@ -40,25 +27,16 @@ replier.reply("확인용4"+ msg);
             .timeout(200000)
             .post();
         
-        json = JSON.parse(response.text());
-        replier.reply(json.generations[0].text);
-        result = json.choices[0].message.content;
-        //console.log(json,resul:t);
-        replier.reply("1 "+response);
-        replier.reply("3 "+result);
+        json = JSON.parse(response.text()); //html 형식의 파일을 json으로 파싱
+        //replier.reply(json.generations[0].text); //json에서 []은 배열을 의미한다. {}은 키 밸류 값
+        messageOut = json.generations[0].text;
+        replier.reply(messageOut);
     }
      catch(e){
          result = e;
          Log.e(json);
          Log.e(json);
-         //replier.reply("확인용2");
-    }
-    //replier.reply("1 "+response);
-    //    replier.reply("2 "+json.generations[text]);
-    //    replier.reply("3 "+result);
-    //replier.reply("4 "+JSON.stringify(json));
-    // return result;
-
+     }
   }
   getKoGPTResponse(msg);
 }
