1d74e5aece7af48e4e820b15558818f1464a6383
Leejinuk123 <50895677+Leejinuk123@users.noreply.github.com>
2023-06-15 14:47:26 +0900

    Update kalingTest.js
    모바일 수정본 json 파싱완료

--- kalingTest.js
+++ kalingTest.js
@@ -1,13 +1,28 @@
+// const scriptName = "고미봇";
 // //카카오링크 관련 모듈 선언부-----------------
-const scriptName = "kakao_link_test";
+//const kalingModule = require('kaling').Kakao();
+//const Kakao = new kalingModule();
+//Kakao.init('2642767733346608749b39371caaabc7');
 const REST_API_KEY = "8efcbd9e5bfe39410994c53a66fa92d7";
+//Kakao.login('leejinouk123@gmail.com', 'djswpsk1!');
+
+
+
+const scriptName = "kakao_link_test";
+// const kalingModule = require('kaling').Kakao();
+// const Kakao = new kalingModule();
+// importClass(org.jsoup.Jsoup);
+
+// Kakao.init(''); // 자바스크립트 키
+// Kakao.login('', '');  // 카카오 계정 아이디와 비밀번호
+
 function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName) {
   function getKoGPTResponse(msg) {
     replier.reply("확인용0");
     let json;
     let result;
     try {
-        
+replier.reply("확인용4"+ msg);
         let data = {
             "max_tokens": 16,
             "temperature": 0.9,
@@ -24,19 +39,24 @@ function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName)
             .ignoreHttpErrors(true)
             .timeout(200000)
             .post();
-
+        
         json = JSON.parse(response.text());
+        replier.reply(json.generations[0].text);
         result = json.choices[0].message.content;
-        console.log(json,result);
-        replier.reply("확인용1");
+        //console.log(json,resul:t);
+        replier.reply("1 "+response);
+        replier.reply("3 "+result);
     }
      catch(e){
          result = e;
-         Log.e(json.id);
-         Log.e(json.id);
-         replier.reply("확인용2");
+         Log.e(json);
+         Log.e(json);
+         //replier.reply("확인용2");
     }
-    replier.reply(JSON.stringify(json));
+    //replier.reply("1 "+response);
+    //    replier.reply("2 "+json.generations[text]);
+    //    replier.reply("3 "+result);
+    //replier.reply("4 "+JSON.stringify(json));
     // return result;
 
   }
