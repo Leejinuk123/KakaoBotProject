8a68f8b3ab4f1eaa097f7a66371c519e919098dc
Leejinuk123 <50895677+Leejinuk123@users.noreply.github.com>
2023-06-21 13:15:52 +0900

    Update lostarkBotEdit.js

--- 로아봇/lostarkBotEdit.js
+++ 로아봇/lostarkBotEdit.js
@@ -36,7 +36,7 @@ function response(room, msg, sender, isGroupChat, replier, imageDB, packageName)
     charName = msg.split(" ");
     //-----------------------------크롤링과 할당부분
     loaInfoHtml = org.jsoup.Jsoup.connect("https://lostark.game.onstove.com/Profile/Character/" + charName[1]).get(); //공식 홈페이지 프로필 html 전체 크롤링
-    const test = org.jsoup.Jsoup.connect("https://loawa.com/char/%EC%9A%B0%EB%B9%84%EC%9A%B1").get();
+    const test = org.jsoup.Jsoup.connect("https://loawa.com/char/우비욱").userAgent("Mozila/5.0").get();
     replier.reply("test");
     dataWrap = loaInfoHtml.select("div.content.content--profile"); //전체 html에서 프로필 정보만 추출
     charImg = dataWrap.select("div.profile-equipment__character").select("img").attr("src"); //프로필 안의 캐릭터 사진 url이 담겨있는 변수
