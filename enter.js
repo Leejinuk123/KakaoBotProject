294a284bf10c8254d69a87eb417950570a0f1587
Leejinuk123 <50895677+Leejinuk123@users.noreply.github.com>
2023-06-23 16:01:19 +0900

    Update enter.js

--- enter.js
+++ enter.js
@@ -4,56 +4,59 @@ function getRoomInfo(link, name) {
   var Executors = java.util.concurrent.Executors;
   var Callable = java.util.concurrent.Callable;
   var Future = java.util.concurrent.Future;
-  function getUrlText(url) {
-    try {
-      return org.jsoup.Jsoup.connect(url).ignoreContentType(true).get().text();
-    }
-    catch (e) {
-      return "";
-    }
-  }
-  var executorService = Executors.newCachedThreadPool();
-  var futures = [];
-  for (var i = 0; i < 5; i++) {
-    var task = new Callable({call: (function (i) {
-      return function () {
-        return getUrlText(urls[i]);
-            };
-    })(i)});
-    var future = executorService.submit(task);
-    futures.push(future);
-  }
-  executorService.shutdown();
-  while (!executorService.isTerminated()) {
-    }
-  var results = [];
-  for (var i = 0; i < futures.length; i++) {
-    var result = futures[i].get();
-    results.push(result);
-  }
-  for (var c in results) {
-    let tar = JSON.parse(results[c]);
-    if (tar.items.length == 0) {
-      result = {"page": 0, "count": 0, "totalCount": 0, "status": 0};
-      break;
-    } else {
-      let b = tar.items.find((obj) => obj.lu === link);
-      if (b == undefined) {
-        continue;
-      } else {
-        result = b;
-        break;
+  
+    function getUrlText(url) {
+      try {
+        return org.jsoup.Jsoup.connect(url).ignoreContentType(true).get().text();
+      }
+      catch (e) {
+        return "";
       }
     }
-  }  
-  return result;
-}
+  
+    var executorService = Executors.newCachedThreadPool();
+    var futures = [];
+    for (var i = 0; i < 5; i++) {
+      var task = new Callable({call: (function (i) {
+        return function () {
+                return getUrlText(urls[i]);
+              };
+      })(i)});
+      var future = executorService.submit(task);
+      futures.push(future);
+    }
+  
+    executorService.shutdown();
+    while (!executorService.isTerminated()) {}
+  
+    var results = [];
+    for (var i = 0; i < futures.length; i++) {
+      var result = futures[i].get();
+      results.push(result);
+    }
+  
+    for (var c in results) {
+      let tar = JSON.parse(results[c]);
+      if (tar.items.length == 0) {
+        result = {"page": 0, "count": 0, "totalCount": 0, "status": 0};
+        break;
+      } else {
+        let b = tar.items.find((obj) => obj.lu === link);
+        if (b == undefined) {
+          continue;
+        } else {
+          result = b;
+          break;
+        }
+      }
+    }  
+    return result;
+  }
 function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName){
   if( msg==("/방정보")){
-    let jsonData = JSON.stringify(getRoomInfo("https://open.kakao.com/o/giQ3rfrf","테스트1"),0,2); //데이터 받아오기
+    let jsonData = JSON.stringify(getRoomInfo("https://open.kakao.com/o/giQ3rfrf","테스트1")); //데이터 받아오기
     if(jsonData == null) return; //예외처리
     
-    let jsonObj = JSON.parse(jsonData.text()); //json으로 파싱
     replier.reply(jsonObj.mcnt, jsonObj.rc);
   }
 }
